dd43db1db789e567acfb7d6854d8f49d4be14466
Enel Almonte <enelramon@hotmail.com>
2026-03-16 20:21:41 -0400

    Agrega data inicial.
    - Backup
    - Script dml

--- AdventureWorksDominicana.Services/CreditCardService.cs
+++ AdventureWorksDominicana.Services/CreditCardService.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Text;
-using AdventureWorksDominicana.Data.Models;
+﻿using AdventureWorksDominicana.Data.Models;
 using Aplicada1.Core;
+using System.Linq.Expressions;
 
 namespace AdventureWorksDominicana.Services;
 
