eddc4f818d3a38a867f62c5fd0690aba5b6e16b3
Fabio <Fabio@DESKTOP-JDI9CML>
2026-03-24 14:31:57 -0400

    Agrega funcionalidad BillOfMaterial y Modifica el Currency
    close #40

---
+++ AdventureWorksDominicana.Services/CurrencyRateService.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq.Expressions;
+using AdventureWorksDominicana.Data.Context;
+using AdventureWorksDominicana.Data.Models;
+using Aplicada1.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorksDominicana.Services;
+
+public class CurrencyRateService(IDbContextFactory<Contexto> DbFactory) : IService<CurrencyRate, int>
+{
+    public async Task<bool> Guardar(CurrencyRate entidad)
+    {
+        entidad.ModifiedDate = DateTime.Now; 
+        if (!await Existe(entidad.CurrencyRateId))
+        {
+            return await Insertar(entidad);
+        }
+        else
+        {
+            return await Modificar(entidad);
+        }
+    }
+
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.CurrencyRates.AnyAsync(e => e.CurrencyRateId == id);
+    }
+
+    public async Task<bool> Insertar(CurrencyRate entidad)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.CurrencyRates.Add(entidad);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(CurrencyRate entidad)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.CurrencyRates.Update(entidad);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<CurrencyRate?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.CurrencyRates.FirstOrDefaultAsync(a => a.CurrencyRateId == id);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.CurrencyRates.Where(a => a.CurrencyRateId == id).ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<CurrencyRate>> GetList(Expression<Func<CurrencyRate, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.CurrencyRates.AsNoTracking().Where(criterio).ToListAsync();
+    }
+}
---
+++ AdventureWorksDominicana.Services/ProductService.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+using AdventureWorksDominicana.Data.Context;
+using AdventureWorksDominicana.Data.Models;
+using Aplicada1.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdventureWorksDominicana.Services;
+
+public class ProductService(IDbContextFactory<Contexto> DbFactory) : IService<Product, int>
+{
+    public async Task<bool> Guardar(Product entidad)
+    {
+        entidad.ModifiedDate = DateTime.Now; 
+
+        if (entidad.ProductId == 0 || !await Existe(entidad.ProductId))
+        {
+            return await Insertar(entidad);
+        }
+        else
+        {
+            return await Modificar(entidad);
+        }
+    }
+
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Products.AnyAsync(e => e.ProductId == id);
+    }
+
+    private async Task<bool> Insertar(Product entidad)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        if (entidad.Rowguid == Guid.Empty)
+        {
+            entidad.Rowguid = Guid.NewGuid();
+        }
+
+        contexto.Products.Add(entidad);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Product entidad)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Products.Update(entidad);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Product?> Buscar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Products.FirstOrDefaultAsync(a => a.ProductId == id);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Products.Where(a => a.ProductId == id).ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<List<Product>> GetList(Expression<Func<Product, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Products.AsNoTracking().Where(criterio).ToListAsync();
+    }
+}
