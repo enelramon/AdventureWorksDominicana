6a054f64ff771c4935f1920bd4c5cab8971006ad
tavarezdiazcristian <tavarezdiazcristian@gmail.com>
2026-03-25 18:56:20 -0700

    Inyecta el Services de ProyectReview en el Program

--- AdventureWorksDominicana.Blazor/Program.cs
+++ AdventureWorksDominicana.Blazor/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddScoped<ShiftService>();
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<ProductCategoryService>();
 builder.Services.AddScoped<SalesTerritoryService>();
-
+builder.Services.AddScoped<ProductReviewService>();
 builder.Services.AddBlazorBootstrap();
 var app = builder.Build();
 
