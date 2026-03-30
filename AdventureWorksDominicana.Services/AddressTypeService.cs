ce8cd751a5bfec358b3dd04734ad5e93a9459649
Tino3q <Hanlexgarcia31@gmail.com>
2026-03-30 17:22:10 -0400

    #42
    Se Han Arreglado Los Pages

--- AdventureWorksDominicana.Services/AddressTypeService.cs
+++ AdventureWorksDominicana.Services/AddressTypeService.cs
@@ -66,8 +66,9 @@ public class AddressTypeService(IDbContextFactory<Contexto> DbFactory) : IServic
         return await contexto.SaveChangesAsync() > 0;
     }
 
-    public Task<List<AddressType>> GetList(Expression<Func<AddressType, bool>> criterio)
+    public async Task<List<AddressType>> GetList(Expression<Func<AddressType, bool>> criterio)
     {
-        throw new NotImplementedException();
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.AddressTypes.Where(criterio).AsNoTracking().ToListAsync();
     }
 }
