27ea104f4a0dea935142e6837ba9b2f33320196a
Tino3q <Hanlexgarcia31@gmail.com>
2026-03-30 00:45:48 -0400

    Se Han Realizado Algunas Correcciones
    Link De Somee:http://adventureworkspruebas.somee.com/ProductModel/Index

--- AdventureWorksDominicana.Blazor/Program.cs
+++ AdventureWorksDominicana.Blazor/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 // Services
+// Services
 builder.Services.AddScoped<CurrencyService>();
 builder.Services.AddBlazoredToast();
 builder.Services.AddScoped<ShipMethodService>();
@@ -25,18 +26,12 @@ builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<ContactTypeService>();
 builder.Services.AddScoped<ProductCategoryService>();
 builder.Services.AddScoped<PhoneNumberTypeService>();
-builder.Services.AddScoped<ShiftService>();
-builder.Services.AddScoped<EmployeeService>();
-builder.Services.AddScoped<ProductCategoryService>();
 builder.Services.AddScoped<SalesTerritoryService>();
 builder.Services.AddScoped<VendorService>();
 builder.Services.AddScoped<ProductDescriptionService>();
 builder.Services.AddScoped<AddressTypeService>();
 builder.Services.AddScoped<ProductModelService>();
-
-
 builder.Services.AddScoped<ShoppingCartItemService>();
-
 
 builder.Services.AddBlazorBootstrap();
 var app = builder.Build();
